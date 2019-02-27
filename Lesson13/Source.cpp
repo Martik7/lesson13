d2dd44609cc86fdcf084d5825010047adf196866
Олжас Садвокасов <@local.itstep>
2019-02-27 19:26:31 +0600

    Добавьте файлы проекта.

---
+++ Lesson13/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <clocale>
+#include <ctime>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	//int x=100, y;
+
+	////неконстатный указатель на неконстантную переменную
+	//int *p1;
+
+	////неконстатный указатель на неконстантную переменную
+	//const int *p2;
+
+	////констатный указатель на неконстантную переменную
+	//int *const p3 = &x;
+
+	////констатный указатель на константную переменную
+	//const int *const p4 = &x;
+
+
+
+	system("pause");
+	return 0;
+}
